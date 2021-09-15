b4222b13ba496d7d3d8ddab0f2f33c2f5886e5cb
warit <warit.boonmasiri@gmail.com>
2021-09-15 23:30:52 +0700

    Add test that fail

--- app_test.go
+++ app_test.go
@@ -4,9 +4,16 @@ import (
  "testing"
 )
 
-func TestInput1ShouldBeDisplay1(t *testing.T) {
+func TestPlus1And2Shouldbe3(t *testing.T) {
      v := Plus(1,2)
      if 3 != v { 
-       t.Error("fizzbuzz of 1 should be '1' but have", v)
+       t.Error("1 + 2 should be 3 but", v)
      }
+}
+
+func TestPlus0and4Shouldbe5(t *testing.T) {
+  v := Plus(0,4)
+  if 5 != v { 
+    t.Error("0 + 4 should be 5 but", v)
+  }
 }
