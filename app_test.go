46e70ae0262868286e500d627125e355c6638e09
warit <warit.boonmasiri@gmail.com>
2021-10-11 21:45:23 +0700

    Add manual trigger

--- app_test.go
+++ app_test.go
@@ -1,19 +1,19 @@
 package main
 
 import (
- "testing"
+	"testing"
 )
 
 func TestPlus1And2Shouldbe3(t *testing.T) {
-     v := Plus(1,2)
-     if 3 != v { 
-       t.Error("1 + 2 should be 3 but", v)
-     }
+	v := Plus(1, 2)
+	if 3 != v {
+		t.Error("1 + 2 should be 3 but", v)
+	}
 }
 
 func TestPlus0and4Shouldbe5(t *testing.T) {
-  v := Plus(0,4)
-  if 5 != v { 
-    t.Error("0 + 4 should be 5 but", v)
-  }
+	v := Plus(0, 4)
+	if 4 != v {
+		t.Error("0 + 4 should be 5 but", v)
+	}
 }
